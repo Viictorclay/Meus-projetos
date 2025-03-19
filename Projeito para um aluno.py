61f45fdc4e354154eb574954a6267df2a251b1c4
Viictorclay <victorcllay.pe@gmail.com>
2025-03-19 20:14:12 -0300

    19/03/2025

--- Projeito para um aluno.py
+++ Projeito para um aluno.py
@@ -5,12 +5,14 @@ print("                ESCOLA JAVALI CANSADO                 ")
 print("------------------------------------------------------")
 aluno = input(" NOME DO ALUNO: ")
 dinenheiro = float(input(" DINHERO: R$"))
-while True:
+
+while True: 
+
     presenca = input(" O ALUNO FEZ TODAS AS PROVAS? ")
     if presenca == "Sim" or presenca == "sim":
-        nota1 = float(input(" PRIMEIRA NOTA: "))
-        nota2 = float(input(" SEGUNDA NOTA: "))
-        nota3 = float(input(" TERCEIRA NOTA: "))
+        nota1 = float(input(" NOTA DA PRIMEIRA PROVA: "))
+        nota2 = float(input(" NOTA DA SEGUNDA PROVA: "))
+        nota3 = float(input(" NOTA DA TERCEIRA PROVA: "))
         media = (nota1 + nota2 + nota3) /3
         print(f" MEDIA: {media}")
         if media >= 6:
@@ -19,74 +21,99 @@ while True:
              print(" ALUNO REPOVADO. ")
         else:
              print(" RESPOSTA INVALIDA, TENTE NOVAMENT. ")
-              
 
     elif presenca == "Não" or presenca == "nao" or presenca == "não":
         fezAL = (input(" MAS CHEGOU A FZER ALGUMA ? "))
         if fezAL == "Sim" or fezAL == "sim":
 
-            #opcoes_validas = ["prieira", "segunda", "terceira"]
+            opcoes_validas = ["prieira", "segunda", "terceira"]
 
-            quais = input(" QUAIS PROVAS FORAM FEITAS? ")#.strip().lower()
-            if quais == "primera" or quais == "Primeira":
-                nota1 = float(input(" NOTA: "))
-                nota2 = 0
-                nota3 = 0
+            quais = input(" QUAIS PROVAS FORAM FEITAS? ").strip().lower()
+            if quais == "primeira":
+                nota1 = float(input(" NOTA DA PRIMEIRA PROVA: "))
+                nota2 = 0 ; print(" NOTA DA SEGUNDA PROVA: -")
+                nota3 = 0 ; print(" NOTA DA TERCEIRA PROVA: -")
                 media = (nota1 + nota2 + nota3) /3
-                print(f" MEDIA {media} ")
-                if media >= 6:
-                        print(" ALUNO APROVADO! ")
-                elif media < 6:
-                        print(" ALUNO REPROVADO. ")
-                else:
-                        print(" RESPOSTA INVALIDA, TENTE NOVAMENTE.")
-
-
-            elif quais == "segunda" or quais == "Segunda":
-                nota1 = 0
-                nota2 = float(input(" NOTA: "))
-                nota3 = 0
-                media = (nota1 + nota2 + nota3) /3
-                print(f" MEDIA: {media}")
+                print(f" MEDIA {media:.1f} ")
                 if media >= 6:
                     print(" ALUNO APROVADO! ")
-                elif media <= 6:
-                    print(" ALUNO REPROVADO. ")
+                elif media < 6:
+                    print(" ALUNO EM RECUPERAÇÃO. ")
                 else:
                     print(" RESPOSTA INVALIDA, TENTE NOVAMENTE.")
 
-                
-            elif quais == "Terceira" or quais == "Terceira":
-                nota1 = 0
-                nota2 = 0
+            elif quais == "segunda":
+                nota1 = 0 ; print(" NOTA DA PRIMEIRA PROVA: -")
+                nota2 = float(input(" NOTA DA SEGUNDA PROVA: "))
+                nota3 = 0 ; print(" NOTA DA TERCEIRA PROVA: -")
+                media = (nota1 + nota2 + nota3) /3
+                print(f" MEDIA: {media:.1f}")
+                if media >= 6:
+                    print(" ALUNO APROVADO! ")
+                elif media < 6:
+                    print(" ALUNO EM RECUPERAÇÃO. ")
+                else:
+                    print(" RESPOSTA INVALIDA, TENTE NOVAMENTE.")
+
+            elif quais == "terceira":
+                nota1 = 0 ; print(" NOTA DA PRIMEIRA PROVA: ")
+                nota2 = 0 ; print(" NOTA DA SEGUNDA PROVA: ")
                 nota3 = float(input(" NOTA: "))
                 media = (nota1 + nota2 + nota3) /3
-                print(f" MEDIA: {media}")
+                print(f" MEDIA: {media:.1f}")
                 if media >= 6:
                     print(" ALUNO APROVADO! ")
-                elif media <= 6:
-                    print(" ALUNO REPROVADO. ")
+                elif media < 6:
+                    print(" ALUNO EM RECUPERAÇÃO. ")
                 else:
                     print(" RESPOSTA INVALIDA, TENTE NOVAMENTE.")
 
-
-            elif quais == "primeira" and quais == "segunda":
-                nota1 = float(input(" NOTA: "))
-                nota2 = float(input(" NOTA: "))
-                nota3 = 0
+            elif "primeira" in quais and "segunda" in quais:
+                nota1 = float(input(" NOTA DA PRIMEIRA PROVA: "))
+                nota2 = float(input(" NOTA DA SEGUNDA PROVA: "))
+                nota3 = 0 ; print(" NOTA DA TERCEIRA PROVA: -")
                 media = (nota1 + nota2 + nota3) /3
-                print(f" MEDIA: {media}")
+                print(f" MEDIA: {media:.1f}")
                 if media >= 6:
                     print(" ALUNO APROVADO! ")
-                elif media <= 6:
-                    print(" ALUNO REPROVADO. ")        
+                elif media < 6:
+                    print(" ALUNO EM RECUPERAÇÃO. ")        
                 else:
                     print(" RESPOSTA INVALIDA, TENTE NOVAMENTE.")  
 
+            elif "primeira" in quais and "terceira" in quais:
+                nota1 = float(input(" NOTA DA PRIMEIRA PROVA: "))
+                nota2 = 0 ; print(" NOTA DA SEGUNDA PROVA: -")
+                nota3 = float(input(" NOTA DA TERCEIRA PROVA: "))
+                media = (nota1 + nota2 + nota3) /3
+                print(f"MEDIA: {media:.1f}")
+                if media >= 6:
+                    print(" ALUNO APROVADO! ")
+                elif media < 6:
+                    print(" ALUNO EM RECUPERÇÃO. ")    
+                else:
+                    print(" RESPOSTA INVALIDA, TENTE NOVAMENTE.")
 
+            elif "segunda" in quais and "terceira" in quais:
+                nota1 = 0 ; print(" NOTA DA PRIMEIRA PROVA: -")
+                nota2 = float(input(" NOTA DA SEGUNDA PROVA: "))
+                nota3 = float(input(" NOTA DA TERCEIRA PROVA: "))
+                media = (nota1 + nota2 + nota3) /3
+                print(f" MEDIA: {media:.1f}")
+            
+                if media >= 6:
+                    print(" ALUNO APROVADO! ")
+                elif media < 6:
+                    print(" ALUNO EM RECUPERAÇÃO. ")
+                else:
+                    print(" RESPOSTA INVALIDA, TENTE NOVAMENTE. ")
             else:
-                print(" RESPOSTA INVALIDA, TENTE NOVAMENTE.")       
+                print(" RESPOSTA INVALIDA, TENTE NOVAMENTE.")
+        #FAZER ALGO COM DINEHRO AQUI        
+
+        elif fezAL == "não" or fezAL == "Não" or fezAL == "nao":
+            print(" ALUNO REPROVADO. ")
         else:
-            print(" RESPOSTA INVALIDA, TENTE NOVAMENTE.")                 
+            print(" RESPOSTA INVALIDA, TENTE NOVAMENTE.")
     else:
-        print(" RESPOSTA INVALIDA, TENTE NOVAMENTE.")                
+        print(" RESPOSTA INVALIDA, TENTE NOVAMENTE.")
